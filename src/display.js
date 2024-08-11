f0f5ef504888b3f944777e061251311d0063ce3e
9insane9 <145199591+9insane9@users.noreply.github.com>
2024-08-11 16:04:59 +0300

    more graphics

--- src/display.js
+++ src/display.js
@@ -6,8 +6,22 @@ import wave from './images/wave1.png'
 import flame1 from './images/flame1.png'
 import flame2 from './images/flame2.png'
 import flame3 from './images/flame3.png'
- 
+import hit1 from './images/markHit1.png'
+import hit2 from './images/markHit2.png'
+import miss1 from './images/markMiss1.png'
+import miss2 from './images/markMiss2.png'
+import miss3 from './images/markMiss3.png'
+import miss4 from './images/markMiss4.png'
+import splash1 from './images/hitSplash1.png'
+import splash2 from './images/hitSplash2.png'
+import splash3 from './images/hitSplash3.png'
+
  export const display = function() {
+
+  const fireFrames = [flame1, flame2, flame3]
+  const hitFrames = [hit1, hit2]
+  const missFrames = [miss1, miss2, miss3, miss4]
+  const splashFrames = [splash1, splash2, splash3]
 
   function generateGrids() {
     const playerGridContainer = document.querySelector(".player-grid")
@@ -27,11 +41,11 @@ import flame3 from './images/flame3.png'
     console.log('Grids created!')
   }
 
-  function startWaterEffect(grid1, grid2) {
+  function startWaterAnimation(grid1, grid2) {
     generateWater(grid1, grid2)
     setInterval(() => {
       swapAlignment(grid1, grid2)
-    }, 1500)
+    }, 1000)
   }
 
   function generateWater(...grids) {
@@ -43,14 +57,13 @@ import flame3 from './images/flame3.png'
         const rowIndex = Math.floor(index / 10)
 
         if (rowIndex % 2 === 0) { //even rows
-            child.style.backgroundImage = `url('${wave}') no-repeat left center`
+          child.style.backgroundImage = `url('${wave}') no-repeat left center`
         } else { // odd rows
-            child.style.backgroundImage = `url('${wave}') no-repeat right center`
+          child.style.backgroundImage = `url('${wave}') no-repeat right center`
         }
 
         child.style.backgroundSize = "contain"
         child.style.backgroundRepeat = "no-repeat"
-        child.style.backgroundColor = 'rgb(89, 89, 255, 0.5)'
       })
     })
   }
@@ -77,110 +90,181 @@ import flame3 from './images/flame3.png'
 
         child.style.backgroundSize = "cover"
         child.style.backgroundRepeat = "no-repeat"
-        child.style.backgroundColor = 'rgb(89, 89, 255, 0.8)' //fill gaps in background
+        // child.style.backgroundColor = 'rgb(89, 89, 255, 0.8)' //fill gaps in background
       })
     })
 
     isLeftAligned = !isLeftAligned
   }  
 
-  function renderShips(gridEl, board) {
 
-    function createImage(src, axis, isStart = false, isLast = false) {
-      const img = document.createElement('img')
-      img.style.zIndex = "5"
-      img.src = src
-      img.style.width = '100%'
-      img.style.height = '100%'
-      img.style.zIndex = '5'
-      if (axis === 'x') {
-        img.style.transform = isLast ? 'rotate(90deg) scaleX(-1)' : 'rotate(90deg)'
-        img.style.transformOrigin = 'center'
-        if (isStart) {
-          img.style.transform = 'rotate(-90deg) scaleX(-1)'
+  function createImage(src, axis, isStart = false, isLast = false) {
+    const img = document.createElement('img')
+    img.style.zIndex = "5"
+    img.src = src
+    img.style.width = '100%'
+    img.style.height = '100%'
+    img.style.zIndex = '5'
+    if (axis === 'x') {
+      img.style.transform = isLast ? 'rotate(90deg) scaleX(-1)' : 'rotate(90deg)'
+      img.style.transformOrigin = 'center'
+      if (isStart) {
+        img.style.transform = 'rotate(-90deg) scaleX(-1)'
+      }
+    } else if (axis === 'y') {
+      img.style.transform = isLast ? 'scaleY(-1)' : ''
+      img.style.transformOrigin = 'center'
+    }
+    return img
+  }
+
+  function renderShip(gridEl, coordinates, ship) {
+    const length = ship.status.length
+
+    coordinates.forEach((coord, index) => {
+      const cell = gridEl.children[coord]
+
+      const shipMarkElement = cell.querySelector(".ship-mark")
+      if (shipMarkElement) { shipMarkElement.remove() }
+
+      if (length === 1) {
+        cell.appendChild(createImage(destroyer, ship.status.axis))
+      } else {
+
+        if (index === 0) { //start
+          cell.appendChild(createImage(shipEnd, ship.status.axis, true, false))
+        } else if (index === coordinates.length - 1) { //end
+          cell.appendChild(createImage(shipEnd, ship.status.axis, false, true))
+        } else { //middle
+          cell.appendChild(createImage(shipMiddle, ship.status.axis))
         }
-      } else if (axis === 'y') {
-        img.style.transform = isLast ? 'scaleY(-1)' : ''
-        img.style.transformOrigin = 'center'
       }
-      return img
-    }
-  
-    function renderShip(shipID, coordinates) {
-      const length = board.fleet[shipID].status.length
-  
-      coordinates.forEach((coord, index) => {
-        const cell = gridEl.children[coord]
+    })
+  }
 
-        if (length === 1) {
-          cell.appendChild(createImage(destroyer, board.fleet[shipID].status.axis))
-        } else {
-  
-          if (index === 0) { //start
-            cell.appendChild(createImage(shipEnd, board.fleet[shipID].status.axis, true, false))
-          } else if (index === coordinates.length - 1) { //end
-            cell.appendChild(createImage(shipEnd, board.fleet[shipID].status.axis, false, true))
-          } else { //middle
-            cell.appendChild(createImage(shipMiddle, board.fleet[shipID].status.axis))
-          }
-      }
-      })
-    }
+  function renderFleet(gridEl, board) {
 
     Array.from(gridEl.children).forEach(child => {
       child.innerHTML = ''
     })
-  
-    ///render
+
     Object.keys(board.fleet).forEach(shipID => {
       const ship = board.fleet[shipID]
       const coordinates = ship.status.position
+      console.log(coordinates)
       if (coordinates && coordinates.length > 0) {
-        renderShip(shipID, coordinates)
+        renderShip(gridEl, coordinates, ship)
       }
     })
   }
 
-  function startFireEffect(gridEl, pos) {
+  //fleet helpers
+
+  
+
+  //animations
+
+  function startFireAnimation(gridEl, pos) {
     const cells = Array.from(gridEl.children)
+
     cells.forEach((cell) => {
       if (cell.getAttribute("data-index") == pos) {
-        const fireImg = document.createElement("img")
-        fireImg.style.width = '100%'
-        fireImg.style.height = '100%'
-        fireImg.style.zIndex = '6'
-
-        cell.appendChild(fireImg)
-
-        fireEffect(fireImg)
+        startAnimation(cell, fireFrames, true, 500, "fire")
       }
     })
   }
 
-  function fireEffect(imgEl) {
-    const fireFrames = [flame1, flame2, flame3]
-    
-    let currentFrame = 0
-    let direction = 1
+  function markHitAndRenderShipAnimation(gridEl, pos, ship, gameRound) {
+    const cells = Array.from(gridEl.children)
 
-    setInterval(() => {
-      currentFrame += direction
+    cells.forEach((cell) => {
+      if (cell.getAttribute("data-index") == pos) {
+        startAnimation(cell, hitFrames, true, 500, "ship-mark", "5")
+        if (ship.status.isSunk) {
 
-      if (currentFrame === fireFrames.length - 1 || currentFrame === 0) {
-          direction *= -1
+          const board = gameRound.boards.ai
+          const shipID = board.boardState[pos]
+          const coordinates = board.fleet[shipID].status.position
+
+          renderShip(gridEl, coordinates, ship)
+        }
       }
+    })
+  }
 
-      imgEl.src = fireFrames[currentFrame]
-      imgEl.style.width = '100%'
-      imgEl.style.height = '100%'
-      imgEl.style.zIndex = '6'
-    }, 500)
+  function markMissedShotAnimation(gridEl, pos) {
+    const cells = Array.from(gridEl.children)
+
+    cells.forEach((cell) => {
+      if (cell.getAttribute("data-index") == pos) {
+        startAnimation(cell, missFrames, true, 500, "missed") //replace with missedFrames
+      }
+    })
+  }
+
+  function splashAnimation(gridEl, pos) {
+    const cells = Array.from(gridEl.children)
+
+    cells.forEach((cell) => {
+      if (cell.getAttribute("data-index") == pos) {
+        startAnimation(cell, splashFrames, false, 500) //replace with splashFrames
+      }
+    })
+  }
+
+
+
+  function startAnimation(parentEl, framesArr, isLoop, interval, className = "no-class", zIndex = '6') {
+    const imgEl = document.createElement("img")
+
+    imgEl.classList.add(className)
+
+    parentEl.appendChild(imgEl)
+
+    if (isLoop) {
+      let currentFrame = 0
+      let direction = 1
+
+      setInterval(() => {
+        currentFrame += direction
+
+        if (currentFrame === framesArr.length - 1 || currentFrame === 0) {
+            direction *= -1
+        }
+
+        imgEl.src = framesArr[currentFrame]
+        imgEl.style.width = '100%'
+        imgEl.style.height = '100%'
+        imgEl.style.zIndex = zIndex
+        imgEl.style.position = 'absolute'
+      }, interval)
+
+    } else { //one time animation
+      let currentFrame = 0
+
+      const intervalId = setInterval(() => {
+        if (currentFrame < framesArr.length) {
+            imgEl.src = framesArr[currentFrame]
+            imgEl.style.width = '100%'
+            imgEl.style.height = '100%'
+            imgEl.style.zIndex = '6'
+            imgEl.style.position = 'absolute'
+            currentFrame++
+        } else {
+            clearInterval(intervalId)
+            imgEl.remove()
+        }
+      }, interval)
+    }
   }
 
   return {
     generateGrids,
-    startWaterEffect,
-    renderShips,
-    startFireEffect
+    startWaterAnimation,
+    renderFleet,
+    startFireAnimation,
+    splashAnimation, 
+    markHitAndRenderShipAnimation, 
+    markMissedShotAnimation
   }
  }
--- src/index.js
+++ src/index.js
@@ -11,14 +11,12 @@ render.generateGrids()
 const playerGridEl = document.querySelector('.player-grid')
 const aiGridEl = document.querySelector('.ai-grid')
 
-render.startWaterEffect(playerGridEl, aiGridEl) //startWaterEffect
-render.renderShips(playerGridEl, gameRound.boards.player)
-
-render.startFireEffect(playerGridEl, 50)
+render.startWaterAnimation(playerGridEl, aiGridEl) //startWaterEffect
+render.renderFleet(playerGridEl, gameRound.boards.player)
 
 document.addEventListener("DOMContentLoaded", () => {
   initEvents(gameRound)
-  render.renderShips(gameRound.boards.player.boardState)
+  render.renderFleet(gameRound.boards.player.boardState)
 })
 
 function initEvents(gameRound) {
@@ -27,7 +25,7 @@ function initEvents(gameRound) {
 
   boardBtn.addEventListener('click', () => {
     gameRound.menu().generateRandomBoard(gameRound.boards.player)
-    render.renderShips(playerGridEl, gameRound.boards.player)
+    render.renderFleet(playerGridEl, gameRound.boards.player)
   })
 
   startBtn.addEventListener('click', () => {
@@ -38,16 +36,67 @@ function initEvents(gameRound) {
 function start() {
   aiGridEl.addEventListener('click', fireShotEvent)
   gameRound.menu().startGame()
+
+  const menu = document.querySelector(".menu") //bye menu
+  menu.style.display = "none"
 }
 
 function fireShotEvent(event) {
-  console.log('Firing shot!')
-  const pos = event.target.getAttribute('data-index');
-  gameRound.playTurn(pos)
+  const pos = Number(event.target.getAttribute('data-index')) //problem: first time duplicate fires at 0 still
+  console.log(pos)
+  const aiShotsReceived = gameRound.boards.ai.shotsReceived
+  const isNewPosition = !aiShotsReceived.includes(pos)
 
-  let gameOver = gameRound.getGameOver()
-  if (gameOver) {
-    aiGridEl.removeEventListener('click', fireShotEvent)
-  }
+  console.log(aiShotsReceived)
+  console.log(isNewPosition)
+
+  if (isNewPosition) {
+    console.log('Firing shot!')
+
+    gameRound.playTurn(pos) 
+
+    updateDisplay(pos)
+
+    let gameOver = gameRound.getGameOver()
+    if (gameOver) {
+      aiGridEl.removeEventListener('click', fireShotEvent)
+
+      const menu = document.querySelector(".menu") //bye menu
+      menu.style.display = "flex"
+    }
+  } else { return }
 }
 
+function updateDisplay(pos) {
+  const playerShotsReceived = gameRound.boards.player.shotsReceived
+  const aiHitPos = playerShotsReceived[playerShotsReceived.length - 1]
+  const aiShipOnBoard = gameRound.boards.ai.boardState[pos]
+  const aiFleet = gameRound.boards.ai.fleet
+  const aiShipInFleet = aiFleet[aiShipOnBoard]
+
+  const playerShipOnBoard = gameRound.boards.player.boardState[aiHitPos]
+  const playerFleet = gameRound.boards.player.fleet
+  const playerShipInFleet = playerFleet[playerShipOnBoard]
+
+  //on hit for player
+  if (aiShipInFleet) {
+    render.startFireAnimation(aiGridEl, pos)
+    render.markHitAndRenderShipAnimation(aiGridEl, pos, aiShipInFleet, gameRound)
+  } else { //on miss for player
+    render.splashAnimation(aiGridEl, pos)
+    setTimeout(() => { render.markMissedShotAnimation(aiGridEl, pos) }, 1510)
+    
+  }
+
+  setTimeout(() => {
+
+    if (playerShipInFleet) { //ai on hit
+      render.startFireAnimation(playerGridEl, aiHitPos)
+    } else { //ai on miss
+      render.splashAnimation(playerGridEl, aiHitPos)
+
+      setTimeout(() => { render.markMissedShotAnimation(playerGridEl, aiHitPos) }, 1510)
+      
+    }
+  }, 1000)
+}
